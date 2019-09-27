b94bf9510d57e232929655781d41aa1659474f19
Andrew Cathcart <andrew.cathcart@salecycle.com>
2019-09-27 22:36:56 +0100

    WIP, need to integrate a database and google sheets

--- commands/add-raid.js
+++ commands/add-raid.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+
 module.exports = {
   name: 'add-raid',
   args: true,
@@ -5,5 +7,31 @@ module.exports = {
   description: 'Schedule a raid given a date argument.',
   execute(message, args) {
     message.channel.send(`Scheduled raid on ${args[0]}.`);
+    console.log(message.guild.emojis);
+
+    const embed = new Discord.RichEmbed()
+      .setColor('#0099ff')
+      .setTitle(`Upcoming raid on ${args[0]}.`)
+      .setURL('https://discord.js.org/')
+      .setAuthor(
+        'Nozdormu the Timeless One',
+        'https://i.imgur.com/X1vRk0Y.jpg',
+        'https://discord.js.org'
+      )
+      .setDescription(
+        'Please react to this message to confirm your attendance!'
+      )
+      .setThumbnail('https://i.imgur.com/kBHGjbS.jpg');
+
+    let sentMessage;
+    message.channel
+      .send(embed)
+      .then(message => {
+        sentMessage = message;
+        message.react('✅').then(() => {
+          sentMessage.react('❌');
+        });
+      })
+      .catch('Failed to add reactions to embedded post.');
   }
 };
